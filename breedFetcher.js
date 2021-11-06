2c54d78172048154e80e29f613e03a140e5952d7
Harry Jiang <harry10291993@hotmail.com>
2021-11-06 17:17:09 -0400

    Refactored breedFetcher into index and add test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,24 @@
-const args = process.argv.splice(2)[0];
+const fetchBreedDescription = function(breedName, callback) {
 
-const request = require('request');
+  const request = require('request');
 
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, response, body) => {
-  console.log('statusCode: ', response && response.statusCode);
-  
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(data[0]['description']);
-})
+  request(url, (error, response, body) => {
+    // console.log('statusCode: ', response && response.statusCode);
+    
+    const data = JSON.parse(body);
+    // console.log(data);
+
+    if (data.length === 0) {
+      error = "Invalid/Non-existent breed is passed in!";
+      callback(error);
+    } else {
+      callback(error, data[0]['description']);
+    }
+
+  });
+
+};
+
+module.exports = {fetchBreedDescription};
